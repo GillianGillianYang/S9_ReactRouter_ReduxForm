eeb78a6f5fea849d189acf64191785a4eae51471
MrRabit <Gillian@GilliantekiMacBook-Air-2.local>
2017-08-25 16:29:49 +0800

    128.rendering a list of posts
    chrome  tool : postman >>
     “POST”  http://reduxblog.herokuapp.com/api/posts?key=CCCIAO1234
    body>raw>json

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions'; 
+import _ from 'lodash';
 
 class PostsIndex extends Component{
     componentDidMount(){
@@ -8,15 +9,36 @@ class PostsIndex extends Component{
         //check chrome -> network > HXR , show an empty array 
     }
 
+    renderPosts(){
+
+        return(
+            _.map(this.props.posts, post =>{
+                return(
+                    <li className="list-group-item" key={post.id}>
+                        {post.title}
+                    </li>
+                );
+            } )
+
+        );
+    }
 
     render(){
+       // console.log(this.props.posts);  //show twice , first empty 
         return(
             <div>
-                Posts Index 
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul> 
             </div>    
         )
     };
 
 }
 
-export default connect(null, {fetchPosts : fetchPosts})(PostsIndex);
+function mapStateToProps(state){
+    return {posts : state.posts} ;
+}
+
+export default connect(mapStateToProps, {fetchPosts : fetchPosts})(PostsIndex);
