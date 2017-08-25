45b60185ef88c498ac52ced6ac46835cab399d5e
MrRabit <Gillian@GilliantekiMacBook-Air-2.local>
2017-08-25 15:55:45 +0800

    127.Action creator  shortcuts
    127.Action creator  shortcuts
    react component life-cycle :
    http://andyyou.logdown.com/posts/178998-reactjs-assembly-operation-and-l
    ife-cycle
    
    life-cycle diagram
    https://cdn-images-1.medium.com/max/1600/0*VoYsN6eq7I_wjVV5.png
    
    fetch post is asynchronous , so  the data show while
    “componentDidMount”
    
    hook action creator (fetchPos) with this component  (PostIndex):
    connect( null , {fetchPos : fetchPos})(PostIndex)
      null-> state

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions'; 
 
 class PostsIndex extends Component{
+    componentDidMount(){
+        this.props.fetchPosts();
+        //check chrome -> network > HXR , show an empty array 
+    }
+
+
     render(){
         return(
             <div>
@@ -11,4 +19,4 @@ class PostsIndex extends Component{
 
 }
 
-export default PostsIndex;
+export default connect(null, {fetchPosts : fetchPosts})(PostsIndex);
